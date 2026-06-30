5252a9dc3d6871f7163dce3a39de93dc8e7c6b1c
JuanCruzSistemas <olguinjuancruz@gmail.com>
2026-06-30 04:58:56 -0300

    feat: mejor lógica en 1019.py

--- 1019.py
+++ 1019.py
@@ -1,8 +1,17 @@
+# N = int(input())
+
+# minutos = N // 60
+# segundos = N - minutos * 60
+# horas = minutos // 60
+# minutos -= horas * 60
+
+# print(f'{horas}:{minutos}:{segundos}')
+
 N = int(input())
 
+horas = N // 3600
+N -= horas * 3600
 minutos = N // 60
-segundos = N - minutos * 60
-horas = minutos // 60
-minutos -= horas * 60
+N -= minutos * 60
 
-print(f'{horas}:{minutos}:{segundos}')
+print(f'{horas}:{minutos}:{N}')
